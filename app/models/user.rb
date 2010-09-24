262eea42c3fb36ad959a623a1ef4458d143ddbae
adwo <adwo.inbox@gmail.com>
2010-09-24 14:13:01 +0400

    email confirmation disabled for clearance

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
   include Clearance::User
+  after_create :confirm_user
   has_attached_file :photo
+  
+  def deliver_confirmation_email
+    # Do Nothing
+  end
+  
+  private
+    def confirm_user
+      self.update_attribute(:email_confirmed, true)
+    end
 end
