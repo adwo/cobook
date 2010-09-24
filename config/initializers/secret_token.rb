d713dcbe59325e8dbdb761764b0320c68735667d
adwo <adwo.inbox@gmail.com>
2010-09-24 12:46:43 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cobook::Application.config.secret_token = '7e3ea30c4adb3544e2c55e3636ece5bfc8e97f1f6e471994f4571c437ed848f584e9d4eca6be59b14c0bd3022d3044164e12eea6efe37c9cae622a8931fb9dfe'
