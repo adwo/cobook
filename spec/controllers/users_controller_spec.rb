24d2b72b6e460ba90a248112e0f3b61161d3f3f8
adwo <adwo.inbox@gmail.com>
2010-09-24 13:29:29 +0400

    registration form test added

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
 
 describe UsersController do
+  render_views
+  
   describe 'GET new' do
-    it 'should be successful' do
+    before do
       get :new
+    end
+    
+    it 'should be successful' do
       response.should be_success
+    end
+    
+    it 'should have registration form' do
+      response.should have_selector('form#new_user')
     end
   end
 end
