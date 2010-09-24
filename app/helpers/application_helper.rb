472c1388e8e75eb79e6a8455c2d71d64dbe66909
adwo <adwo.inbox@gmail.com>
2010-09-24 14:01:49 +0400

    title helper added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def title(type=:page)
+    @detail_title = @title if @detail_title.nil?
+    if type == :detail
+      @detail_title
+    else
+      @title
+    end
+  end
 end
