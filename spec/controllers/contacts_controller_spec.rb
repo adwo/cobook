11fb86dee3f16fe3785e6293de4580a450c83e00
adwo <adwo.inbox@gmail.com>
2010-09-25 15:05:04 +0400

    contacts index action added

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe ContactsController do
-
+  describe 'GET :index' do
+    before do
+      get :index
+    end
+    
+    it { should respond_with(:success) }
+  end
 end
