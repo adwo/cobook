87d49664504679fe07451e02250309e3d26f1779
Bulbakha Svyatoslav <adwo.inbox@gmail.com>
2010-09-27 15:53:40 +0400

    ContactsController spec refactored

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -7,19 +7,19 @@ describe ContactsController do
     @group = Factory.create(:group)
   end
   
-  def do_get
-    get :index, :group_id => @group
+  def do_get(action)
+    get action, :group_id => @group
   end
   
   it 'should redirect unsigned user to sign in page' do
-    do_get
+    do_get(:index)
     response.should redirect_to(sign_in_path)
   end
     
-  describe 'signed in' do
+  describe 'GET :index' do
     before do
       sign_in
-      do_get
+      do_get(:index)
     end
     
     it { should respond_with(:success) }
